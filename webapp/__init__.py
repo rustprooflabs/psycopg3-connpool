44e29d859bd076f79f077858f57af88434f58790
Ryan Lambert <ryan@rustprooflabs.com>
2021-04-12 14:29:22 -0600

    Remove now-unnecessary psycopg3 logging controls

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,13 +10,8 @@ app.config['WTF_CSRF_ENABLED'] = True
 
 if config.APP_DEBUG:
     log_level = logging.DEBUG
-    logging.getLogger('psycopg3').setLevel(logging.INFO)
 else:
     log_level = logging.INFO
-    # Don't display INFO from psycopg3, too noisy for app logs
-    # If addressed this can be removed: https://github.com/psycopg/psycopg3/issues/43
-    logging.getLogger('psycopg3').setLevel(logging.WARN)
-
 
 logging.basicConfig(filename=config.LOG_PATH,
                     level=log_level,
